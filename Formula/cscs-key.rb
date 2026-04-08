a20a39e31723678c08aeaae102df67ef524bc2d0
Matteo Seclì <secli.matteo@gmail.com>
2026-04-08 14:49:07 +0200

    Simplify cscs-key formula

--- Formula/cscs-key.rb
+++ Formula/cscs-key.rb
@@ -2,6 +2,7 @@ class CscsKey < Formula
   desc "CLI tool to manage SSH keys for the Swiss National Supercomputing Centre (CSCS)"
   homepage "https://github.com/eth-cscs/cscs-key"
   version "1.0.0"
+  revision 1
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/eth-cscs/cscs-key/releases/download/1.0.0/cscs-key-1.0.0-macos-aarch64.tar.gz"
@@ -13,30 +14,8 @@ class CscsKey < Formula
     sha256 "b0a31197d9d6e7d65281924b2312b2bb397f52909eb47da88c037fe435358e12"
   end
 
-  BINARY_ALIASES = {
-    "macos-aarch64":     {},
-    "linux-x86_64": {},
-  }.freeze
-
-  def target_triple
-    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
-    os = OS.mac? ? "macos" : "linux"
-
-    "#{os}-#{cpu}"
-  end
-
-  def install_binary_aliases!
-    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
-      dests.each do |dest|
-        bin.install_symlink bin/source.to_s => dest
-      end
-    end
-  end
-
   def install
     bin.install "cscs-key" if OS.mac? && Hardware::CPU.arm?
     bin.install "cscs-key" if OS.linux? && Hardware::CPU.intel?
-
-    install_binary_aliases!
   end
 end
